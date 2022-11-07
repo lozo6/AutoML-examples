ee3cb9ca826f62d107415e9f6abc13bb6e228391
root <root@lozo>
2022-11-07 18:48:53 -0500

    added necessary libraries

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
 import pandas as pd
+from sklearn.metrics import accuracy_score
+from sklearn.model_selection import train_test_split
+from supervised.automl import AutoML
 
 df_sparcs = pd.read_csv('https://raw.githubusercontent.com/hantswilliams/HHA-507-2022/main/autoML/datasets/data_sparcs.csv')
+
+df_sparcs.columns
